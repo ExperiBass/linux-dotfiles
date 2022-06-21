93a674e941ec67e821d38364dfb857f4e62b002c
Gingka/Ginger Pepper <gingkagingerpepper@gmail.com>
2022-06-21 00:22:24 -0400

    beefed it up

--- .bin/restore.sh
+++ .bin/restore.sh
@@ -18,14 +18,55 @@ mv ./tmp/.git ~
 rmdir ./tmp
 git checkout master
 
+# TODO: Figure out how to edit kernel args in /etc/default/grub
+
 echo "Restoring files..."
 cp -ruv $backup /
+
+echo "Creating swapfile..."
+dd if=/dev/zero of=/swapfile bs=10M count=2000 # 20gb, ram+hibernate
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+
 echo "Installing packages..."
 yay -S --needed --nocleanmenu --nodiffmenu --noeditmenu --noremovemake \
     --noupgrademenu --answerclean=N --answerdiff=N --answerupgrade=Y --noconfirm \
     - < $pkglist
+
 echo "Installing AUR packages..."
 yay -S --needed --nocleanmenu --nodiffmenu --noeditmenu --noremovemake \
     --noupgrademenu --answerclean=N --answerdiff=N --answerupgrade=Y --noconfirm \
     - < $aurpkglist
+
+echo "Compiling kernel..."
+# https://wiki.t2linux.org/guides/kernel/
+export MAKEFLAGS=-j$(nproc)
+cd ~/Builds
+mkdir -p kernel
+cd kernel
+git clone --depth=1 https://github.com/Redecorating/mbp-16.1-linux-wifi patches
+source patches/PKGBUILD
+wget https://www.kernel.org/pub/linux/kernel/v${pkgver//.*}.x/linux-${pkgver}.tar.xz
+tar xf $_srcname.tar.xz
+cd $_srcname
+git clone --depth=1 https://github.com/t2linux/apple-bce-drv drivers/staging/apple-bce
+git clone --depth=1 https://github.com/Redecorating/apple-ib-drv drivers/staging/apple-ibridge # Redecoratings patch works
+for patch in ../patches/*.patch; do
+    patch -Np1 < $patch
+done
+zcat /proc/config.gz > .config
+make olddefconfig
+scripts/config --module apple-ibridge
+scripts/config --module apple-bce
+make
+make modules_install
+make install
+mkinitcpio -k /boot/vmlinuz -c /etc/mkinitcpio.conf -g /boot/initramfs.img
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Removing unneeded/broken packages..."
+yay -Rns --noconfirm xf86-video-intel linux-t2 linux-t2-headers
+yay -Rdd pipewire wireplumber pipewire-alsa
+
 echo "Done."
